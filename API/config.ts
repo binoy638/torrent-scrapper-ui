946e77eeff12ec75937769377cb0e39262cb4a0d
binoy638 <binoy638@gmail.com>
2023-03-12 17:43:11 +0530

    fix: base url

--- API/config.ts
+++ API/config.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // export const API = axios.create({ baseURL: process.env.NEXT_PUBLIC_BASE_URL });
 export const API = axios.create({
-  baseURL: "https://n19l07htrd.execute-api.ap-southeast-1.amazonaws.com/dev/",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
